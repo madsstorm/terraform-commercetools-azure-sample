e5ab2e6b9e6090d45b022ebc8251ad4f6bc0ecd0
Mads Storm Hansen <madsstorm@gmail.com>
2021-02-04 13:23:33 +0100

    Function App

---
+++ commercetools-integrations/modules/common/azure/function-app/function-app.tf
@@ -0,0 +1,40 @@
+resource "azurerm_resource_group" "this" {
+    name = var.resource_group_name
+    location = var.location    
+}
+
+resource "azurerm_storage_account" "this" {
+    name = var.storage_account_name
+    resource_group_name = azurerm_resource_group.this.name
+    location = azurerm_resource_group.this.location
+    account_tier = var.storage_account_tier
+    account_replication_type = var.storage_account_replication_type    
+}
+
+resource "azurerm_app_service_plan" "this" {
+    name = var.service_plan_name
+    location = azurerm_resource_group.this.location
+    resource_group_name = azurerm_resource_group.this.name
+    kind = var.service_plan_kind
+
+    sku {
+        tier = var.service_plan_tier
+        size = var.service_plan_size
+    }
+}
+
+resource "azurerm_function_app" "this" {
+    name = var.function_app_name
+    location = azurerm_resource_group.this.location
+    resource_group_name = azurerm_resource_group.this.name
+    app_service_plan_id = azurerm_app_service_plan.this.id
+    storage_account_name = azurerm_storage_account.this.name
+    storage_account_access_key = azurerm_storage_account.this.primary_access_key
+    version = "3"
+       
+    connection_string {
+      name = "ServiceBusconnection"
+      type = "ServiceBus"
+      value = var.servicebus_connection_string
+    }
+}
---
+++ commercetools-integrations/modules/common/azure/function-app/input.tf
@@ -0,0 +1,16 @@
+variable "location" {}
+
+variable "resource_group_name" {}
+
+variable "storage_account_name" {}
+variable "storage_account_tier" {}
+variable "storage_account_replication_type" {}
+
+variable "service_plan_name" {}
+variable "service_plan_kind" {}
+variable "service_plan_tier" {}
+variable "service_plan_size" {}
+
+variable "function_app_name" {}
+
+variable "servicebus_connection_string" {}
