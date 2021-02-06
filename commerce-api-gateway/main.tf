cfc2b7c82ca7b796e7ccb221fcce3c8428779481
Mads Storm Hansen <madsstorm@gmail.com>
2021-02-06 14:13:06 +0100

    APIM Diagnostics

--- commerce-api-gateway/main.tf
+++ commerce-api-gateway/main.tf
@@ -41,13 +41,77 @@ resource "azurerm_resource_group" "this" {
     location = var.location
 }
 
+resource "azurerm_application_insights" "example" {
+  name = "appi-${var.project}-${var.environment}-${random_string.resource_code.result}"
+  location = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+  application_type = "web"  
+}
+
 resource "azurerm_api_management" "this" {
   name = "apim-${var.project}-${var.environment}-${random_string.resource_code.result}"
   location = azurerm_resource_group.this.location
   resource_group_name = azurerm_resource_group.this.name
-
   publisher_email = "madsstorm@gmail.com"
   publisher_name = "Mads Storm Hansen"
+  sku_name = var.api_management_sku_name  
+}
 
-  sku_name = var.api_management_sku_name 
+resource "azurerm_api_management_logger" "this" {
+  name = "log-${var.project}-${var.environment}-${random_string.resource_code.result}"
+  api_management_name = azurerm_api_management.this.name
+  resource_group_name = azurerm_resource_group.this.name
+
+  application_insights {
+    instrumentation_key = azurerm_application_insights.this.instrumentation_key
+  }
+}
+
+resource "azurerm_api_management_diagnostic" "this" {
+  identifier = "applicationinsights"
+  resource_group_name = azurerm_resource_group.this.name
+  api_management_name = azurerm_api_management.this.name
+  api_management_logger_id = azurerm_api_management_logger.this.id
+
+  sampling_percentage = 10
+  always_log_errors = true
+  log_client_ip = true
+  verbosity = "verbose"
+  http_correlation_protocol = "W3C"
+
+  frontend_request {
+    body_bytes = 32
+    headers_to_log = [
+      "content-type",
+      "accept",
+      "origin",
+    ]
+  }
+
+  frontend_response {
+    body_bytes = 32
+    headers_to_log = [
+      "content-type",
+      "content-length",
+      "origin",
+    ]
+  }
+
+  backend_request {
+    body_bytes = 32
+    headers_to_log = [
+      "content-type",
+      "accept",
+      "origin",
+    ]
+  }
+
+  backend_response {
+    body_bytes = 32
+    headers_to_log = [
+      "content-type",
+      "content-length",
+      "origin",
+    ]
+  }
 }
