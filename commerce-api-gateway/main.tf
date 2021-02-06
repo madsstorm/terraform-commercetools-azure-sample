0eba8f4eb6a9355912edcb5e2ece77dd1b8cdd58
Mads Storm Hansen <madsstorm@gmail.com>
2021-02-06 14:35:25 +0100

    Azure provider version

--- commerce-api-gateway/main.tf
+++ commerce-api-gateway/main.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "2.45.1"
+      version = "2.46.1"
     }
     random = {
       source = "hashicorp/random"
