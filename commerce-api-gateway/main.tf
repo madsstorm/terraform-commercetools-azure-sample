b5932395a6dbf51a4b7b7494144409fc1e5e1a88
Mads Storm Hansen <madsstorm@gmail.com>
2021-02-06 14:19:38 +0100

    APIM

--- commerce-api-gateway/main.tf
+++ commerce-api-gateway/main.tf
@@ -73,45 +73,45 @@ resource "azurerm_api_management_diagnostic" "this" {
   api_management_name = azurerm_api_management.this.name
   api_management_logger_id = azurerm_api_management_logger.this.id
 
-  sampling_percentage = 10
-  always_log_errors = true
-  log_client_ip = true
-  verbosity = "verbose"
-  http_correlation_protocol = "W3C"
+  # sampling_percentage = 10
+  # always_log_errors = true
+  # log_client_ip = true
+  # verbosity = "verbose"
+  # http_correlation_protocol = "W3C"
 
-  frontend_request {
-    body_bytes = 32
-    headers_to_log = [
-      "content-type",
-      "accept",
-      "origin",
-    ]
-  }
+  # frontend_request {
+  #   body_bytes = 32
+  #   headers_to_log = [
+  #     "content-type",
+  #     "accept",
+  #     "origin",
+  #   ]
+  # }
 
-  frontend_response {
-    body_bytes = 32
-    headers_to_log = [
-      "content-type",
-      "content-length",
-      "origin",
-    ]
-  }
+  # frontend_response {
+  #   body_bytes = 32
+  #   headers_to_log = [
+  #     "content-type",
+  #     "content-length",
+  #     "origin",
+  #   ]
+  # }
 
-  backend_request {
-    body_bytes = 32
-    headers_to_log = [
-      "content-type",
-      "accept",
-      "origin",
-    ]
-  }
+  # backend_request {
+  #   body_bytes = 32
+  #   headers_to_log = [
+  #     "content-type",
+  #     "accept",
+  #     "origin",
+  #   ]
+  # }
 
-  backend_response {
-    body_bytes = 32
-    headers_to_log = [
-      "content-type",
-      "content-length",
-      "origin",
-    ]
-  }
+  # backend_response {
+  #   body_bytes = 32
+  #   headers_to_log = [
+  #     "content-type",
+  #     "content-length",
+  #     "origin",
+  #   ]
+  # }
 }
