438f03cc8610134a2eb804d2a59e402e806bb60a
Mads Storm Hansen <madsstorm@gmail.com>
2021-02-09 23:02:41 +0100

    API logging

--- commerce-api-gateway/modules/api-diagnostic/api-diagnostic.tf
+++ commerce-api-gateway/modules/api-diagnostic/api-diagnostic.tf
@@ -5,45 +5,9 @@ resource "azurerm_api_management_api_diagnostic" "this" {
   api_name                 = var.api_name
   api_management_logger_id = var.api_management_logger_id
 
-  sampling_percentage       = 5.0
+  sampling_percentage       = 0
   always_log_errors         = true
-  log_client_ip             = true
+  log_client_ip             = false
   verbosity                 = "verbose"
   http_correlation_protocol = "W3C"
-
-  frontend_request {
-    body_bytes = 32
-    headers_to_log = [
-      "content-type",
-      "accept",
-      "origin",
-    ]
-  }
-
-  frontend_response {
-    body_bytes = 32
-    headers_to_log = [
-      "content-type",
-      "content-length",
-      "origin",
-    ]
-  }
-
-  backend_request {
-    body_bytes = 32
-    headers_to_log = [
-      "content-type",
-      "accept",
-      "origin",
-    ]
-  }
-
-  backend_response {
-    body_bytes = 32
-    headers_to_log = [
-      "content-type",
-      "content-length",
-      "origin",
-    ]
-  }
 }
