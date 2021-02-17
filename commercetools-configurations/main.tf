e0d04f5cab7cdd430dca7b64aea85a02c28e0aae
Mads Storm Hansen <madsstorm@gmail.com>
2021-02-17 13:05:57 +0100

    Tax

--- commercetools-configurations/main.tf
+++ commercetools-configurations/main.tf
@@ -20,6 +20,19 @@ resource "commercetools_project_settings" "project" {
   countries  = ["DK"]
 }
 
+resource "commercetools_tax_category" "standard" {
+  name = "Standard tax category"
+  key  = "standard-tax-category"
+}
+
+resource "commercetools_tax_category_rate" "standard-tax-category-DA" {
+  tax_category_id   = "${commercetools_tax_category.standard.id}"
+  name              = "25% Moms"
+  amount            = 0.25
+  included_in_price = true
+  country           = "DA"
+}
+
 module "product-types" {
   source = "./modules/product-types"
 }
