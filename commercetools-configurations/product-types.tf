cfaa2ee8e2a0100acba13de22ea7a9a4868484b3
Mads Storm Hansen <madsstorm@gmail.com>
2021-02-26 14:55:20 +0100

    Refactor

--- commercetools-configurations/product-types.tf
+++ commercetools-configurations/product-types.tf
@@ -3,3 +3,39 @@ resource "commercetools_product_type" "mobile_broadband_product_type" {
   description = "Mobile broadband"
   key         = "mobile-broadband"
 }
+
+resource "commercetools_product_type" "modem_product_type" {
+  name        = "Modem"
+  description = "Modem"
+  key         = "modem"
+
+  attributes {
+    name = "short-description"
+    label = {
+      en = "Short description"
+      da = "Kort beskrivelse"
+    }
+    required = false
+    constraint = "None"
+    searchable = true
+    input_hint = "MultiLine"
+    type {
+      name = "ltext"
+    }
+  }
+
+    attributes {
+    name = "general-description"
+    label = {
+      en = "General description"
+      da = "General beskrivelse"
+    }
+    required = false
+    constraint = "None"
+    searchable = true
+    input_hint = "MultiLine"
+    type {
+      name = "ltext"
+    }
+  }
+}
