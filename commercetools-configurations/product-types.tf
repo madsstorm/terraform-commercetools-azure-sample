d887d7d7966ad75d298af01a22f20b1a54b707b4
Mads Storm Hansen <madsstorm@gmail.com>
2021-03-01 12:20:46 +0100

    Prices and relations

--- commercetools-configurations/product-types.tf
+++ commercetools-configurations/product-types.tf
@@ -4,16 +4,32 @@ resource "commercetools_product_type" "mobile_broadband_subscription" {
   key         = "mobile-broadband-subscription"
 
   attribute {
-    name = "product_prices_relations"
+    name = "product_prices_reference"
     label = {
-      da = "Priser og relationer"
-      en = "Prices and relations"
+      da = "Produkt priser"
+      en = "Product prices"
     }
     required   = false
+    constraint = "SameForAll"
     searchable = false
     type {
-      name           = "nested"
-      type_reference = commercetools_product_type.product_prices_relations.id
+      name              = "reference"
+      reference_type_id = "key-value-document"
+    }
+  }
+
+  attribute {
+    name = "product_relations_reference"
+    label = {
+      da = "Produkt relationer"
+      en = "Product relations"
+    }
+    required   = false
+    constraint = "SameForAll"
+    searchable = false
+    type {
+      name              = "reference"
+      reference_type_id = "key-value-document"
     }
   }
 }
@@ -22,33 +38,6 @@ resource "commercetools_product_type" "mobile_broadband_hardware" {
   name        = "Broadband hardware"
   description = "Hardware for mobile broadband subscriptions"
   key         = "mobile-broadband-hardware"
-}
-
-resource "commercetools_product_type" "service_option" {
-  name        = "Service option"
-  description = "Options for services"
-  key         = "service-option"
-}
-
-resource "commercetools_product_type" "product_prices_relations" {
-  name        = "Product prices and relations"
-  description = "Prices and relations for products"
-  key         = "product-prices-relations"
-
-  attribute {
-    name = "variant_prices_reference"
-    label = {
-      da = "Variant priser"
-      en = "Variant prices"
-    }
-    required   = false
-    constraint = "None"
-    searchable = false
-    type {
-      name              = "reference"
-      reference_type_id = "key-value-document"
-    }
-  }
 
   attribute {
     name = "product_prices_reference"
@@ -66,13 +55,34 @@ resource "commercetools_product_type" "product_prices_relations" {
   }
 
   attribute {
-    name = "variant_relations_reference"
+    name = "product_relations_reference"
     label = {
-      da = "Variant relationer"
-      en = "Variant relations"
+      da = "Produkt relationer"
+      en = "Product relations"
     }
     required   = false
-    constraint = "None"
+    constraint = "SameForAll"
+    searchable = false
+    type {
+      name              = "reference"
+      reference_type_id = "key-value-document"
+    }
+  }
+}
+
+resource "commercetools_product_type" "service_option" {
+  name        = "Service option"
+  description = "Options for services"
+  key         = "service-option"
+
+  attribute {
+    name = "product_prices_reference"
+    label = {
+      da = "Produkt priser"
+      en = "Product prices"
+    }
+    required   = false
+    constraint = "SameForAll"
     searchable = false
     type {
       name              = "reference"
