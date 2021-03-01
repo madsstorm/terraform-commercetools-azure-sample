d86cc53ae2bb26dc0b37c71ff208f98623e69ba4
Mads Storm Hansen <madsstorm@gmail.com>
2021-03-01 11:43:26 +0100

    Prices and relations

--- commercetools-configurations/product-types.tf
+++ commercetools-configurations/product-types.tf
@@ -2,6 +2,20 @@ resource "commercetools_product_type" "mobile_broadband_subscription" {
   name        = "Mobile broadband subscription"
   description = "Subscriptions for mobile broadband"
   key         = "mobile-broadband-subscription"
+
+  attribute {
+    name = "product_prices_relations"
+    label = {
+      da = "Priser og relationer"
+      en = "Prices and relations"
+    }
+    required   = false
+    searchable = false
+    type {
+      name = "nested"
+      type_reference = commercetools_product_type.product_prices_relations.id
+    }
+  }
 }
 
 resource "commercetools_product_type" "mobile_broadband_hardware" {
@@ -14,4 +28,70 @@ resource "commercetools_product_type" "service_option" {
   name        = "Service option"
   description = "Options for services"
   key         = "service-option"
+}
+
+resource "commercetools_product_type" "product_prices_relations" {
+  name         = "Product prices and relations"
+  descriptions = "Prices and relations for products"
+  key          = "product-prices-relations"
+
+  attribute {
+    name = "variant_prices_reference"
+    label = {
+      da = "Variant priser"
+      en = "Variant prices"
+    }
+    required   = false
+    constraint = "None"
+    searchable = false
+    type {
+      name              = "reference"
+      reference_type_id = "key-value-document"
+    }
+  }
+
+  attribute {
+    name = "product_prices_reference"
+    label = {
+      da = "Produkt priser"
+      en = "Product prices"
+    }
+    required   = false
+    constraint = "SameForAll"
+    searchable = false
+    type {
+      name              = "reference"
+      reference_type_id = "key-value-document"
+    }
+  }
+
+  attribute {
+    name = "variant_relations_reference"
+    label = {
+      da = "Variant relationer"
+      en = "Variant relations"
+    }
+    required   = false
+    constraint = "None"
+    searchable = false
+    type {
+      name              = "reference"
+      reference_type_id = "key-value-document"
+    }
+  }
+
+  attribute {
+    name = "product_relations_reference"
+    label = {
+      da = "Produkt relationer"
+      en = "Product relations"
+    }
+    required   = false
+    constraint = "SameForAll"
+    searchable = false
+    type {
+      name              = "reference"
+      reference_type_id = "key-value-document"
+    }
+  }
 }
