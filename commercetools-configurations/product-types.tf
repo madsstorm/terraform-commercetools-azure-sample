77d7cd20f81f99268ff285f396d25871237afca5
Mads Storm Hansen <madsstorm@gmail.com>
2021-04-12 14:08:26 +0200

    Update product-types.tf

--- commercetools-configurations/product-types.tf
+++ commercetools-configurations/product-types.tf
@@ -1,1 +1,26 @@
+resource "commercetools_product_type" "smartphone" {
+  name        = "Smartphone"
+  description = "Smartphone"
+  key         = "smartphone"
 
+  attribute {
+      name = "brand"
+      label = {
+          da = "Brand"
+          en = "Brand"
+      }
+      required = true
+      type {
+          name = "enum"
+          values = {
+              samsung = "Samsung"
+              huawei = "Huawei"
+              sony = "Sony"
+              apple = "Apple"
+              oneplus = "OnePlus"
+              motorola = "Motorola"
+          }
+      }
+  }
+
+}
