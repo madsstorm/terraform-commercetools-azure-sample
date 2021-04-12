b9709ab2aaa90655826f7aa285da6221176e29a6
Mads Storm Hansen <madsstorm@gmail.com>
2021-04-12 10:15:32 +0200

    Update project.tf

--- commercetools-configurations/project.tf
+++ commercetools-configurations/project.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 resource "commercetools_project_settings" "project" {
-  name       = "${var.projectname}"
+  name       = var.projectname
   currencies = ["DKK"]
   languages  = ["en", "da"]
   countries  = ["DK"]
