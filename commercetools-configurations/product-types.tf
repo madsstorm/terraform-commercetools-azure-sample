894e5a97c226497e7773fc4763e7974bbd9579ea
Mads Storm Hansen <madsstorm@gmail.com>
2021-04-12 14:56:50 +0200

    Update product-types.tf

--- commercetools-configurations/product-types.tf
+++ commercetools-configurations/product-types.tf
@@ -26,13 +26,38 @@ resource "commercetools_product_type" "smartphone" {
   }
 
   attribute {
+      name = "Storage"
+      label = {
+          da = "Storage"
+          en = "Storage"
+      }
+      required = false
+      constraint = "CombinationUnique"
+      searchable = false
+      type {
+          name = "enum"
+          values = {
+              32 = "32 GB"
+              64 = "64 GB"
+              128 = "128 GB"
+              256 = "256 GB"
+              512 = "512 GB"
+              1024 = "1 TB"
+              2048 = "2 TB"
+              4096 = "4 TB"
+              8192 = "8 TB"              
+          }
+      }
+  }
+
+  attribute {
       name = "ColourDescription"
       label = {
           da = "Farve beskrivelse"
           en = "Colour description"
       }
       required = false
-      constraint = "None"
+      constraint = "CombinationUnique"
       searchable = true
       type {
           name = "ltext"
