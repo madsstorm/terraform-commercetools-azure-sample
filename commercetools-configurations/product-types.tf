cd66219cb2574769582bcc6a7a66efad0932ea60
Mads Storm Hansen <madsstorm@gmail.com>
2021-04-26 15:40:53 +0200

    Relations

--- commercetools-configurations/product-types.tf
+++ commercetools-configurations/product-types.tf
@@ -18,34 +18,6 @@ resource "commercetools_product_type" "device" {
   }
 
   attribute {
-      name = "StorageGB"
-      label = {
-          da = "Storage GB"
-          en = "Storage GB"
-      }
-      required = false
-      constraint = "None"
-      searchable = true
-      type {
-          name = "number"
-      }
-  }
-
-  attribute {
-      name = "ScreenSizeInches"
-      label = {
-          da = "Skærmstørrelse tommer"
-          en = "Screen size inches"
-      }
-      required = false
-      constraint = "None"
-      searchable = true
-      type {
-          name = "number"
-      }
-  }
-
-  attribute {
       name = "ColorDescription"
       label = {
           da = "Farvebeskrivelse"
@@ -71,6 +43,64 @@ resource "commercetools_product_type" "device" {
     type {
       name = "money"
     }
+  }
+
+  attribute {
+    name = "RelationsForProduct"
+    label = {
+      da = "Relationer for produkt"
+      en = "Relations for product"
+    }
+    required = false
+    constraint = "SameForAll"
+    searchable = false
+    type {
+      name = "nested"
+      type_reference = commercetools_product_type.product_relation.id
+    }
+  }
+
+  attribute {
+    name = "RelationsForVariant"
+    label = {
+      da = "Relationer for variant"
+      en = "Relations for variant"
+    }
+    required = false
+    constraint = "None"
+    searchable = false
+    type {
+      name = "nested"
+      type_reference = commercetools_product_type.product_relation.id
+    }
+  }
+  
+  attribute {
+      name = "StorageGB"
+      label = {
+          da = "Storage GB"
+          en = "Storage GB"
+      }
+      required = false
+      constraint = "None"
+      searchable = true
+      type {
+          name = "number"
+      }
+  }
+
+  attribute {
+      name = "ScreenSizeInches"
+      label = {
+          da = "Skærmstørrelse tommer"
+          en = "Screen size inches"
+      }
+      required = false
+      constraint = "None"
+      searchable = true
+      type {
+          name = "number"
+      }
   }
 
   attribute {
@@ -134,10 +164,89 @@ resource "commercetools_product_type" "accessory" {
       name = "money"
     }
   }
+
+  attribute {
+    name = "RelationsForProduct"
+    label = {
+      da = "Relationer for produkt"
+      en = "Relations for product"
+    }
+    required = false
+    constraint = "SameForAll"
+    searchable = false
+    type {
+      name = "nested"
+      type_reference = commercetools_product_type.product_relation.id
+    }
+  }
+
+  attribute {
+    name = "RelationsForVariant"
+    label = {
+      da = "Relationer for variant"
+      en = "Relations for variant"
+    }
+    required = false
+    constraint = "None"
+    searchable = false
+    type {
+      name = "nested"
+      type_reference = commercetools_product_type.product_relation.id
+    }
+  }
 }
 
 resource "commercetools_product_type" "subscription" {
   name        = "Subscription"
   description = "Subscription"
   key         = "subscription"
+}
+
+resource "commercetools_product_type" "product_relation" {
+  name = "Product Relation"
+  description = "Product Relation"
+  key = "productrelation"
+
+  attribute {
+    name  = "Product"
+    label = {
+      da = "Produkt"
+      en = "Product"
+    }
+    required = true
+    constraint = "None"
+    searchable = false
+    type {
+      name = "reference"
+      reference_type_id = "product"
+    }
+  }
+
+  attribute {
+    name  = "VariantId"
+    label = {
+      da = "Variant Id"
+      en = "Variant Id"
+    }
+    required = false
+    constraint = "None"
+    searchable = false
+    type {
+      name = "number"
+    }
+  }
+
+  attribute {
+    name = "RelationType"
+    label = {
+      da = "Relationstype"
+      en = "Relation type"
+    }
+    required = true
+    constraint = "None"
+    searchable = false
+    type {
+      name = "text"
+    }
+  }
 }
