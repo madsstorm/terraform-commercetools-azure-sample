865268a5a0e091ef4feea71b2116ad26fc964a11
Mads Storm Hansen <madsstorm@gmail.com>
2021-05-07 12:01:34 +0200

    Lineitem group

---
+++ commercetools-configurations/lineitem.tf
@@ -0,0 +1,24 @@
+resource "commercetools_type" "lineitem_group" {
+    key = "lineitem-group"
+    name = {
+        da = "Lineitem group"
+        en = "Lineitem group"
+    }
+    description = {
+        da = "Group lineitems in a Cart or Order"
+        en = "Group lineitems in a Cart or Order"
+    }
+
+    resource_type_ids = ["line-item","custom-line-item"]
+
+    field {
+        name = "groupid"
+        label = {
+            da = "Group Id"
+            en = "Group Id"
+        }
+        type {
+            name = "Number"
+        }
+    }
+}
