cdf704b68f22ffcb738e91ba2634ea2263e77759
Mads Storm Hansen <madsstorm@gmail.com>
2021-06-10 11:08:32 +0200

    azurerm provider version

--- commercetools-subscriptions/main.tf
+++ commercetools-subscriptions/main.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "2.50.0"
+      version = "2.62.1"
     }
     commercetools = {
       source  = "labd/commercetools"
