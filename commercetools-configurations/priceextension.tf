1a8b240a7d29ce9ba5d3aea40944c42a98e3851f
Mads Storm Hansen <madsstorm@gmail.com>
2021-09-21 12:47:37 +0200

    price ext dummy fields

--- commercetools-configurations/priceextension.tf
+++ commercetools-configurations/priceextension.tf
@@ -17,14 +17,91 @@ resource "commercetools_type" "price_extension" {
     }
   }
 
-#  field {
-#    name     = "Campaign"
-#    required = false
-#    label = {
-#      da = "Kampagne"
-#    }
-#    type {
-#      name = "String"
-#    }
-#  }
+ field {
+   name     = "Campaign"
+   required = false
+   label = {
+     da = "Kampagne"
+   }
+   type {
+     name = "String"
+   }
+ }
+
+ field {
+   name     = "SomeNumber"
+   required = false
+   label = {
+     da = "SomeNumber"
+   }
+   type {
+     name = "Number"
+   }
+ }
+
+ field {
+   name     = "SomeEnum"
+   required = false
+   label = {
+     da = "SomeEnum"
+   }
+   type {
+     name = "Enum"
+     values = {
+       monday  = "Monday"
+       tuesday = "Tuesday"
+     }
+   }
+ }
+
+ field {
+   name     = "SomeLocalizedEnum"
+   required = false
+   label = {
+     da = "SomeLocalizedEnum"
+   }
+   type {
+      name = "LocalizedEnum"
+      localized_value {
+        key = "phone"
+        label = {
+          da = "Telefon"
+        }
+      }
+      localized_value {
+        key = "skype"
+        label = {
+          da = "Skype"
+        }
+      }
+   }
+ }
+
+ field {
+   name     = "SomeStringSet"
+   required = false
+   label = {
+     da = "SomeStringSet"
+   }
+   type = {
+      name = "Set"
+      element_type {
+        name = "String"
+      }
+    }
+ }
+
+ field {
+    name  = "SomeProductRefSet"
+    label = {
+      da = "SomeProductRefSet"
+    }
+    type {
+      name = "Set"
+      element_type {
+        name              = "Reference"
+        reference_type_id = "product"
+      }
+    }
+  }
 }
