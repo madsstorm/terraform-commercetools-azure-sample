26608bebd56e11ecb6c64c5aaa9129cf45c78500
Mads Storm Hansen <madsstorm@gmail.com>
2021-09-22 13:32:22 +0200

    order state machine

---
+++ commercetools-configurations/order-states.tf
@@ -0,0 +1,21 @@
+resource "commercetools_state" "order_accepted_oms" {
+  key = "order-accepted-oms"
+  type = "OrderState"
+  name = {
+      da = "Order accepted by OMS"
+  }
+  description = {
+    da = "The order was accepted by Order Management System."
+  }
+}
+
+resource "commercetools_state" "order_rejected_oms" {
+  key = "order-rejected-oms"
+  type = "OrderState"
+  name = {
+      da = "Order rejected by OMS"
+  }
+  description = {
+    da = "The order was rejected by Order Management System."
+  }
+}
