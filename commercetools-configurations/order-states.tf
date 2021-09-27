bc6de4670184254ae9823588aedcee3da9bb8326
Mads Storm Hansen <madsstorm@gmail.com>
2021-09-27 09:35:34 +0200

    Cleanup price extension fields

--- commercetools-configurations/order-states.tf
+++ commercetools-configurations/order-states.tf
@@ -2,7 +2,7 @@ resource "commercetools_state" "order_open" {
   key = "order-open"
   type = "OrderState"
   name = {
-      da = "Order open"
+    da = "Order open"
   }
   description = {
     da = "The order is open."
@@ -13,7 +13,7 @@ resource "commercetools_state" "order_accepted_oms" {
   key = "order-accepted-oms"
   type = "OrderState"
   name = {
-      da = "Order accepted by OMS"
+    da = "Order accepted by OMS"
   }
   description = {
     da = "The order was accepted by Order Management System."
@@ -24,7 +24,7 @@ resource "commercetools_state" "order_rejected_oms" {
   key = "order-rejected-oms"
   type = "OrderState"
   name = {
-      da = "Order rejected by OMS"
+    da = "Order rejected by OMS"
   }
   description = {
     da = "The order was rejected by Order Management System."
